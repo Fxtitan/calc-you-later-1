3a885efcdc28c6d68c4007f15322ae39e87b4559
Sharod Bailey <Sharod.Bailey@codeimmersives.comy>
2020-06-23 14:35:55 -0400

    done

--- calculate-front-end.js
+++ calculate-front-end.js
@@ -1,7 +1,14 @@
 /*********************************************
  * YOUR CODE HERE!                           *
  ********************************************/
+const calculate = require('./calculate-back-end.js');
+const number1 = getInput(0);
+const number2 = getInput(2);
+const operand = getInput(1);
+calculate(number1, number2, operand);
 
+
+console.log(calculate(number1,number2, operand));
 
 
 
